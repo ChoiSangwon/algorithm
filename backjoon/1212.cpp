02f7d5421cdd5c28e8563c539c52af2626118511
fdsa20040200 <fdsa200@naver.com>
2022-07-20 20:12:39 +0900

    add 1212

--- backjoon/1212.cpp
+++ backjoon/1212.cpp
@@ -1,18 +1,15 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<iostream>
+using namespace std;
+string arr;
+string a1[8]={"000","001","010","011","100","101","110","111"};
+string a2[8]={"0","1","10","11","100","101","110","111"};
 int main(){
-    char N[6];
-    int arr[8],b,mok,num,nmg;
-    scanf("%s",N);
-    unsigned long n = strtoul(N, NULL, 8);
-    b=-1;
-    do{
-        b++;
-        mok=num/2;
-        nmg=num-mok*2;
-        arr[b]=nmg;
-    num=mok;
-    }while(mok!=0);
-    for(int i=b;i>=0;i--)
-        printf("%d",arr[i]);
+    cin>>arr;
+    for(int i=0;i<arr.size();i++){
+        int N=arr[i]-'0';
+        if(i==0)
+            cout<<a2[N];
+        else
+            cout<<a1[N];
+    }
 }
