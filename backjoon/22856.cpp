1918f6e73f26ea93b43fbaf6e890fb6916034113
fdsa20040200 <fdsa200@naver.com>
2022-07-21 20:20:16 +0900

    add 22856

--- backjoon/22856.cpp
+++ backjoon/22856.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
+#include<map>
 using namespace std;
+map<int,pair<int,int> > tree;
 int N;
-int main(){
-    cin>>N;
+int res=0;
+int d=0;
+void solve(int node,bool check) {
+    res++;
+	if (tree[node].first != -1) {
+		solve(tree[node].first,false);
+	}
+	if (tree[node].second != -1) {
+        if(check)
+            d++;
+		solve(tree[node].second,check);
+	}
     
+}
+
+int main(){
+    int a,b,c;
+    cin>>N;
+    for(int i=0;i<N;i++){
+        cin>>a>>b>>c;
+        tree[a].first=b;
+        tree[a].second=c;
+    }
+    solve(1,true);
+    cout<<2*(res-1)-d<<endl;
 }
