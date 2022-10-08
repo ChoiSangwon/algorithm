3eefb3531e9068fcffdc572ca8b2186d0145dd91
fdsa20040200 <fdsa200@naver.com>
2022-10-08 13:26:10 +0900

    ㅁ

--- backjoon/2852.py
+++ backjoon/2852.py
@@ -1,28 +1,25 @@
 N = int(input())
-count=[]
-time=[]
+arr=[]
 for i in range(N):
     a,tmp = input().split()
-    count.append(int(a))
-    time.append(int(tmp[:2])*60+int(tmp[3:]))
-count.append(0)
-time.append(2880)
+    arr.append((int(a),int(tmp[:2])*60+int(tmp[3:])))
+arr.append((0,2880))
 A1,B1=0,0
 wintime=[0,0]
 b=[]
-for i in range(len(count)):   
+for i in range(len(arr)):   
     if A1>B1:
-        t1 = time[i]
+        t1 = arr[i][1]
         t2 = b
         wintime[0] += t1-t2
     elif B1>A1:
-        t1 = time[i]
+        t1 = arr[i][1]
         t2 = b
         wintime[1] += t1-t2
-    b=time[i]
-    if(count[i]==1):
+    b=arr[i][1]
+    if(arr[i][0]==1):
         A1+=1
-    elif(count[i]==2):
+    elif(arr[i][0]==2):
         B1+=1
 for i in range(2):
     print(str((wintime[i]//60)).zfill(2)+":"+str((wintime[i]%60)).zfill(2))
