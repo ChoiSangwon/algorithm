09ccb575d635c9f23c3fb972eb752c1897c983d3
fdsa20040200 <fdsa200@naver.com>
2023-01-30 09:55:09 +0900

    add : 2751.py

--- backjoon/2751.py
+++ backjoon/2751.py
@@ -1,8 +1,9 @@
 import sys
-N=int(input())
-arr=[]
-for i in range(N):
-    arr.append(int(sys.stdin.readline()))
-arr.sort()
-for j in arr:
-    print(j)
+arr = [0]*10001
+n= int(input())
+
+for i in range(n):
+    arr[int(sys.stdin.readline())]+=1
+for i in range(10000):
+    for j in range(arr[i]):
+        print(i)
