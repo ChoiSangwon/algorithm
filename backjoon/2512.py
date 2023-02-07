3db5f84186db3b5b0199b0510d004d0572a53e86
fdsa20040200 <fdsa200@naver.com>
2023-02-07 11:50:51 +0900

    add : 2512.py

---
+++ backjoon/2512.py
@@ -0,0 +1,25 @@
+n= int(input())
+arr =list(map(int,input().split()))
+cnt = int(input())
+
+if(sum(arr)<=cnt):
+    print(max(arr))
+else:
+    res=0
+    start = 0
+    end = max(arr)
+    mid=(start+end)//2
+    while(start<=end):
+        mid=(start+end)//2  
+        tmp=0
+        for i in range(n):
+            if arr[i]>=mid:
+                tmp+=mid
+            else:
+                tmp+=arr[i]
+        if(tmp>cnt):
+            end=mid-1
+        else:
+            start=mid+1
+            res=mid
+    print(res)
